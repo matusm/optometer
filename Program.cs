833f93591b4a6ae1c9e6b766e0e937d69d28efb0
Michael Matus <michael@matus.at>
2021-07-17 23:06:39 +0200

    CommandlineParser bug fixed

--- Program.cs
+++ Program.cs
@@ -9,23 +9,18 @@ namespace optometer
 {
     class Program
     {
-        //readonly static string fatSeparator = new string('=', 80);
-        //readonly static string thinSeparator = new string('-', 80);
+        readonly static string fatSeparator = new string('=', 80);
+        readonly static string thinSeparator = new string('-', 80);
 
         public static void Main(string[] args)
         {
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
-            string fatSeparator = new string('=', 80);
-            string thinSeparator = new string('-', 80);
 
             DateTime timeStamp = DateTime.UtcNow;
 
             Options options = new Options();
-
-            Console.WriteLine(options.LogFileName);
-            Console.WriteLine(options.MaximumSamples);
-            Console.WriteLine(options.Port);
-            Console.WriteLine(options.UserComment);
+            if (!CommandLine.Parser.Default.ParseArgumentsStrict(args, options))
+                Console.WriteLine("*** ParseArgumentsStrict returned false");
 
             var streamWriter = new StreamWriter(options.LogFileName, true);
             var device = new P9710(options.Port);
